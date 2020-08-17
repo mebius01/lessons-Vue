58b46f8f97156343d58215461283c49eefcd42a0
iv <consmebius@gmail.com>
2020-08-17 23:39:04 +0300

    d-7

---
+++ lesson2vuex/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <h1>{{ name }}</h1>
+    <Header />
+  </div>
+</template>
+
+<script>
+import Header from './components/Header'
+export default {
+  name: "App",
+  data() {
+    return {
+      name: "Test Vuex"
+    }
+  },
+  components: {
+    Header
+  }
+
+};
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin: 60px auto;
+}
+</style>
