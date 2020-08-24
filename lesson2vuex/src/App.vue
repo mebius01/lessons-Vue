2a6e8f028c255241a817f1459898adc913087aaf
iv <consmebius@gmail.com>
2020-08-24 23:16:32 +0300

    d-12

--- lesson2vuex/src/App.vue
+++ lesson2vuex/src/App.vue
@@ -1,20 +1,73 @@
 <template>
   <div id="app">
+    <template>
+      <div>
+        <p v-if="STATUS_RESPONSE.error">{{STATUS_RESPONSE.error}}</p>
+        <p v-else>{{STATUS_RESPONSE.status}}</p>
+      </div>
+    </template>
+    <input type="text" placeholder="search..." v-model="search" @input="onChangeInput"/>
+    <button @click="CHANGE_SHOW_LIST">Show List Country</button> <span>{{SHOW_LIST}}</span>
+    <br>
+    Search: {{SEARCH}}
+    <br>
+    <button @click="CHANGE_COUNTER_PLUS">+ 1</button>
+    <Counter />
+    <button @click="CHANGE_COUNTER_MINUS">- 1</button>
+    <ul class="results-list">
+        <li v-for="(item, index) in RESULTS" :key="index">
+          <a>
+              {{item.Country}}
+          </a>
+        </li>
+      </ul>
 
   </div>
 </template>
 
 <script>
-import { mapActions } from 'vuex'
+import Counter from './components/Counter'
+import { mapActions, mapGetters } from 'vuex'
 export default {
   name: "App",
+  components: {
+    Counter
+  },
   data() {
-    return {}
+    return {
+      search: '',
+    }
+  },
+  computed: {
+    ...mapGetters("covid", ["ARR_COUNTRIES", // список городов
+                            "STATUS_RESPONSE", // статус и ошибки ответов сервера
+                            "SEARCH",
+                            "RESULTS",
+                            "SHOW_LIST"
+    ]),
+    
+  },
+  methods: {
+    ...mapActions("covid", ["GET_SUMMARY",
+                            "GET_SEARCH",
+                            "GET_RESULTS",
+                            "CHANGE_SHOW_LIST",
+                            "CHANGE_COUNTER_PLUS",
+                            "CHANGE_COUNTER_MINUS"
+    ]),
+
+    onChangeInput() {
+      this.GET_RESULTS()
+      this.GET_SEARCH(this.search)
+    },
+
+    filterResults() {
+      this.GET_RESULTS()
+      },
   },
   mounted() {
-    ...mapActions({
-      "GET_SUMMARY"
-    })
+    this.GET_SUMMARY(),
+    this.GET_RESULTS()
   }
 };
 </script>
--- lesson2vuex/src/store/modules/covid.js
+++ lesson2vuex/src/store/modules/covid.js
@@ -1,27 +1,96 @@
-import axios from "axios"
+import axios from "axios";
 export default {
   namespaced: true,
   state: {
     rootUrl: "https://api.covid19api.com/summary",
-    search: null,
+    search: "",
     results: [],
     btnShowListCountries: false,
     arrDataCountries: null,
     objOneCountry: null,
-    arrowCounter: -1,
+    arrowCounter: 0,
+    statusResponse: {
+      error: null,
+      status: null
+    }
   },
-  getters: {},
+  getters: {
+    ARR_COUNTRIES(state) {
+      return state.arrDataCountries;
+    },
+    STATUS_RESPONSE(state) {
+      return state.statusResponse;
+    },
+    RESULTS(state) {
+      return state.results
+    },
+    SEARCH(state) {
+      return state.search
+    },
+    SHOW_LIST(state) {
+      return state.btnShowListCountries
+    },
+    COUNTER(state) {
+      return state.arrowCounter
+    }
+  },
   mutations: {
-    SET_COUNTRIES: (state, data) => {
-      state.arrDataCountries = data
+    CREATE_ARR_COUNTRIES: (state, list) => {
+      state.arrDataCountries = list
+    },
+    CREATE_STATUS_RESPONSE: (state, status) => {
+      state.statusResponse.status = status
+    },
+    CREATE_ERROR_RESPONSE: (state, error) => {
+      state.statusResponse.error = error
+    },
+    CREATE_SEARCH: (state, payload) => {
+      state.search = payload
+    },
+    CREATE_RESULTS: (state, payload) => {
+      state.results = payload
+    },
+    CREATE_SHOW_LIST: (state, payload) => {
+      state.btnShowListCountries = payload
+    },
+    CREATE_COUNTER: (state, payload) => {
+      state.arrowCounter = payload
     }
   },
   actions: {
-    GET_SUMMARY({ commit }) {
-      return axios.get("https://api.covid19api.com/summary")
-        .then(response => {
-          commit("SET_COUNTRIES", response)
-        })
+    GET_SUMMARY({ state, commit }) {
+      axios.get(state.rootUrl)
+      .then(response => {
+        let resp = response
+        commit("CREATE_ARR_COUNTRIES", resp.data.Countries)
+        commit("CREATE_STATUS_RESPONSE", resp.status)
+      })
+      .catch(error => {
+        commit("CREATE_ERROR_RESPONSE", error)
+      })
+    },
+
+    GET_SEARCH({commit}, search) {
+      commit("CREATE_SEARCH", search)
+    },
+
+    GET_RESULTS({commit, getters}) {
+      let results = getters.ARR_COUNTRIES.filter(item => item.Country.toLowerCase().indexOf(getters.SEARCH.toLowerCase()) > -1)
+      commit("CREATE_RESULTS", results)
+    },
+
+    CHANGE_SHOW_LIST({commit, getters}) {
+      let showList = !getters.SHOW_LIST
+      commit("CREATE_SHOW_LIST", showList)
+    },
+    CHANGE_COUNTER_PLUS({commit, getters}) {
+      let c = getters.COUNTER+1
+      commit("CREATE_COUNTER", c)
+    },
+    CHANGE_COUNTER_MINUS({commit, getters}) {
+      let c = getters.COUNTER-1
+      commit("CREATE_COUNTER", c)
     }
   }
 }
+  // state.arrDataCountries.filter(item => item.Country.toLowerCase().indexOf(state.search.toLowerCase()) > -1)
